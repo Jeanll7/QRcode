e94eb8d5f822f580f0879bdd2fabf07461ee8216
devjean <jeancarloleall@gmail.com>
2022-09-30 13:35:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <input 
+        className='input' 
+        placeholder="Digite seu link..."
+        type="text" 
+      />
+    </div>
+  )
+}
+
+export default App
